a095c9abf4db67685e4447d3632a5945a9fc87b3
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2024-04-04 01:11:25 +0200

    st1

---
+++ 1st_Course/Q2/Databases/U3/Content/1.DLL_comands.sql
@@ -0,0 +1,20 @@
+--Create database
+mysql -u [user] -p
+
+mysql> CREATE DATABASE testdb;
+
+
+mysql> SHOW CREATE DATABASE testdb;
+
+
+mysql> SHOW databases;
+
+
+mysql> USE testdb;
+
+--Drop database
+
+mysql> DROP DATABASE testdb;
+
+--cretate table
+
