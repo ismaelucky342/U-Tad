4084ecd4ac334a8398aa2b90bffd478118b6dc33
Ismael <ismaelHC342@gmail.com>
2024-04-26 18:38:24 +0200

    26

--- 1st_Course/Q2/Databases/U3/Content/1.DLL_comands.sql
+++ 1st_Course/Q2/Databases/U3/Content/1.DLL_comands.sql
@@ -18,3 +18,35 @@ mysql> DROP DATABASE testdb;
 
 --cretate table
 
+CREATE TABLE table_name (column1_def, column2_def..., table_constraints) 
+
+
+--Drop table 
+
+DROP TABLE table_name, table2; 
+
+
+------------------------USER SQL----------------------------------------------------------------
+
+-- Create user 
+
+CREATE USER name_account IDENTIFIED 'PSWD';
+
+-- Grant all privileges
+
+GRANT ALL PRIVILEGES ON name_bd.* TO user_name@hostname; 
+
+-- update 
+
+FLUSH PRIVILEGES; 
+
+-- Show changes 
+
+SHOW GRANTS FOR user_name@hostname; 
+
+-- Drop user 
+
+DROP USER name_account;
+
+
+
