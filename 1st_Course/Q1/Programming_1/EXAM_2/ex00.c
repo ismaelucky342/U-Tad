2e62c86e2441d97f3e64b19751d3f9e15d5b576c
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2024-07-03 17:56:31 +0200

    exam

--- 1st_Course/Q1/Programming_1/EXAM_2/ex00.c
+++ 1st_Course/Q1/Programming_1/EXAM_2/ex00.c
@@ -2,102 +2,94 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NUM_ELEMENTOS 12
+#define NUM_PREMIOS 12
 #define NUM_TERMINACIONES 10
- 
-void leeNumeros(int num_premiados[], int num_elementos);
-void calculaTerminaciones(int* terminaciones, int* num_premiados, int num_elementos); 
-int maximoNumeroPremiado(int num_premiados[], int num_elementos);
-int terminacionMasPopular(int terminaciones[], int num_elementos);
 
-	
-int main(int argc, char** argv)
+int leernumero(int premios[], int elementos);
+int calcularTerminacion(int* terminaciones,int premios[], int elementos);
+int maxnum(int premios[], int elementos);
+int terminacionP(int* terminaciones, int elementos);
+
+int main()
 {
 	int i, j;
-	int num_premiados[NUM_ELEMENTOS];
-	int numMasAlto=0;
-	int terminaciones[NUM_TERMINACIONES]={0,0,0,0,0,0,0,0,0,0};		
-	
-	printf("Introduce 12 números entre 0 y 90.000. Deben ser los premiados en los últimos 12 sorteos\n");
-	leeNumeros(num_premiados, NUM_ELEMENTOS);
-	
-	printf("\n==> Los números premiados en los últimos 12 sorteos son:\n");
-	
-	for (i=0; i<NUM_ELEMENTOS; i++)
-	{
-		 printf("%d	", num_premiados[i] );
-	}
-	printf("\n");	
-	calculaTerminaciones(terminaciones, num_premiados, NUM_ELEMENTOS); 
-	
-	for (i=0; i<NUM_TERMINACIONES; i++)
-	{
-		printf("Numeros terminados en %d: %d\n", i, terminaciones[i]);
-	}
-	int termPopular = terminacionMasPopular(terminaciones, NUM_TERMINACIONES);
-	printf("==> La terminación más popular en estos años ha sido el: %d\n", termPopular); 
-	
-	numMasAlto= maximoNumeroPremiado(num_premiados, NUM_ELEMENTOS);
-	printf("\n==>El número premiado más alto premiado en estos últimos años ha sido el: %d\n", numMasAlto);
+	int numpremios[NUM_PREMIOS];
+	int terminaciones[NUM_TERMINACIONES];
+	int maxnum = 0;
 
+	printf("Ingrese los numeros premiados en los ultimos 12 años: \n");
+	leernumero(numpremios, NUM_PREMIOS);
+
+	printf("los numeros almacenados son: \n");
+	for(i = 0; i < NUM_PREMIOS; i++)
+	{
+		printf("numero %d: %d \n", i+1, numpremios[i]);
+	}
+	return 0;
 }
 
-void leeNumeros(int num_premiados[], int num_elementos)
+int leernumero(int premios[], int elementos)
 {
-	int res;
-	
-	for (int i=0; i<num_elementos; i++)
+	int aux;
+	for (int i = 0; i < elementos; i++)
 	{
-		printf("Introduce el número: %d\n", i+1);
-		do{
-			 res=scanf("%d", &num_premiados[i]);
-			 while (getchar()!='\n');
-			 if (num_premiados[i] <= 0 || num_premiados[i]>90000) printf("El número no es correcto. Intentalo de nuevo\n");
-			 if (res!=1) printf("El dato no es válido\n");
-		}while (num_premiados[i] <= 0 || num_premiados[i] > 90000 || res!=1);
-	}
-}
-
-void calculaTerminaciones(int terminaciones[], int num_premiados[], int num_elementos)
-{
-	int cifra;
-		
-	for (int i=0; i<num_elementos; i++)
-	{
-		cifra=num_premiados[i]%10;
-		terminaciones[cifra]++;
-	}
-	return;
-}
-
-int terminacionMasPopular(int terminaciones[], int num_elementos)
-{
-	int numMax, popular;
-	
-	numMax=terminaciones[0];
-	for (int i=1; i<num_elementos; i++)
-	{
-		if (terminaciones[i]>numMax){
-				numMax=terminaciones[i];
-				popular=i;
+		printf("Introduzca valor %d: \n", i + 1);
+		aux = 0;
+		while (aux != 1 || premios[i] >= 90000 || premios[i] <= 0)
+		{
+			aux = scanf("%d", &premios[i]); 
+			while (getchar() != '\n'); // Limpiar el buffer de entrada
+			
+			if(premios[i] > 90000 || premios[i] <= 0)
+			{
+				printf("Introduzca un valor correcto en el rango de 1 a 90000\n");
+			}
+			else if (aux != 1)
+			{
+				printf("Valor no valido\n");
+			}
 		}
 	}
-	
-	return popular;
-	
+	return 0;
 }
 
-
-int maximoNumeroPremiado(int num_premiados[], int num_elementos)
+int calcularTerminacion(int* terminaciones,int premios[], int elementos)
 {
-	int maximo;
-	
-	maximo=num_premiados[0];
-	for (int i=1; i<num_elementos; i++)
+	int aux; 
+
+	for (int i = 0; i < NUM_PREMIOS; i++)
 	{
-		if (num_premiados[i]>maximo){
-				maximo=num_premiados[i];
+		aux = premios[i] % 10; 
+		terminaciones[aux]++; 
+	}
+	return 0; 
+}
+
+int maxnum(int premios[], int elementos)
+{
+	int max; 
+
+	max = premios[0]; 
+	for(int i = 0; i < elementos; i++)
+	{
+		if(premios[i] > max)
+			max = premios[i]; 
+	}
+	return max; 
+}
+
+int terminacionP(int* terminaciones, int elementos)
+{
+	int pop, elm; 
+
+	elm = terminaciones[0]; 
+	for(int i = 0; i < elementos; i++)
+	{
+		if (terminaciones[i] > elm)
+		{
+			elm = terminaciones[i]; 
+			pop = i; 
 		}
 	}
-		return maximo;
+	return pop; 
 }
