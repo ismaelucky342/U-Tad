f19640861700eb4419102edc4b4dd43876b27bf9
ismaelucky342 <ismaelhc342@gmail.com>
2024-10-16 23:24:08 +0200

    update java

--- 2nd_Course/Q1/POO/Unit_1/src/6.Examples/ex00.java
+++ 2nd_Course/Q1/POO/Unit_1/src/6.Examples/ex00.java
@@ -2,28 +2,21 @@
 Escribir un programa en Java que calcule el producto de tres números enteros introducidos por el usuario.
 */
 
-import  java.util.Scanner; 
+import java.util.Scanner;
 
 public class ex00 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, n3, result; 
 
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int num1, num2, num3, resultado; 
+        System.out.println("Introduce first number: ");
+        n1 = sc.nextInt(); 
+        System.out.println("Introduce second number: ");
+        n2 = sc.nextInt(); 
+        System.out.println("Introduce third number: ");
+        n3 = sc.nextInt(); 
 
-        System.out.println("Insert first digit: ");
-        num1 = entrada.nextInt();
-
-        System.out.println("Insert second digit: ");
-        num2 = entrada.nextInt();
-
-        System.out.println("Insert third digit: ");
-        num3 = entrada.nextInt();
-
-        resultado = num1 * num2 * num3; 
-
-        System.out.println("The product is: " + resultado);
-
+        result = n1 * n2 * n3;
+        System.out.println("The result is: " + result);
     }
-
-
 }
--- 2nd_Course/Q1/POO/Unit_1/src/6.Examples/ex01.java
+++ 2nd_Course/Q1/POO/Unit_1/src/6.Examples/ex01.java
@@ -27,4 +27,3 @@ public class ex01 {
         scanner.close();
     }
 }
-
