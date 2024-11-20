73d8d5eb059a61833a5d18b760287f94fce651a8
ismaelucky342 <ismaelhc342@gmail.com>
2024-11-20 01:15:38 +0100

    java

--- 2nd_Course/Q1/POO/Unit_3/src/1.inheritance/Perro.java
+++ 2nd_Course/Q1/POO/Unit_3/src/1.inheritance/Perro.java
@@ -1,6 +1,9 @@
 public class Perro extends Animal {
+    /**
+     * 
+     */
     @Override
     public void hacerSonido() {
-    System.out.println("El perro ladra.");
+        System.out.println("El perro ladra.");
     }
 }
